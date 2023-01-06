b64735de80da5eea558b3f0216c6418343bf2192
Jared Endicott <Jedijde@gmail.com>
2023-01-05 21:18:35 -0500

    Add project files.

---
+++ Question 4/main.cpp
@@ -0,0 +1,47 @@
+// Section 20
+// Challenge 4
+// Identifying palindrome strings using a stack and queue
+#include <cctype>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+bool is_palindrome(const std::string& s)
+{
+    std::stack<char> s1;
+    std::queue<char> q1;
+
+    for (char i : s)
+        if (isalpha(i)) {
+            q1.push(toupper(i));
+            s1.push(toupper(i));
+        }
+
+    while (!s1.empty()){
+        if (q1.front() != s1.top())
+            return false;
+        else {
+            q1.pop();
+            s1.pop();
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
+   
+    std::cout << std::boolalpha;
+    std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+    for(const auto& s : test_strings) {
+        std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
+}
